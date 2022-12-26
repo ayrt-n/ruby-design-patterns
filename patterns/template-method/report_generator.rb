163514bc6c1158333ea25585631c9b67fc4f6d03
ayrton <ayrton.parkinson1@gmail.com>
2022-12-26 12:51:48 -0500

    Create simple Report generator

---
+++ patterns/template-method/report_generator.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Report
+  attr_reader :title, :text
+
+  def initialize
+    @title = 'Monthly Report'
+    @text = ['Things are going,', 'really, really well.']
+  end
+
+  def output_report
+    puts '<html>'
+    puts '  <head>'
+    puts "    <title>#{title}</title>"
+    puts '  </head>'
+    puts '  <body>'
+    text.each do |line|
+      puts "    <p>#{line}</p>"
+    end
+    puts '  </body>'
+    puts '</html>'
+  end
+end
+
+report = Report.new
+report.output_report
