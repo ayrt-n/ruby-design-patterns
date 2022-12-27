82af45f978c7807b17df3ab34fea49a9fd73a6d2
ayrton <ayrton.parkinson1@gmail.com>
2022-12-27 09:39:19 -0500

    Add example of observer pattern using code blocks

---
+++ patterns/observer/employee_payroll_code_block.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative './subject'
+
+# The Observer Pattern
+
+# Employee Class
+class Employee
+  include Subject
+
+  attr_accessor :name, :title, :salary
+
+  def initialize(name, title, salary)
+    super()
+    @name = name
+    @title = title
+    @salary = salary
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+end
+
+# Example code
+fred = Employee.new('Fred', 'Crane Operator', 30_000.0)
+fred.add_observer do |changed_employee|
+  puts "#{changed_employee.name}'s new salary is #{changed_employee.salary}"
+end
+
+fred.salary = 35_000.0
