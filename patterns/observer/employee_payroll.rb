c1e3dd8d1bee403d9cd01d686c71b6321332f686
ayrton <ayrton.parkinson1@gmail.com>
2022-12-27 09:16:43 -0500

    Add example of observer pattern

---
+++ patterns/observer/employee_payroll.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# The Observer Pattern
+
+# Employee Class
+class Employee
+  attr_reader :name, :salary
+  attr_accessor :title
+
+  def initialize(name, title, salary)
+    @name = name
+    @title = title
+    @salary = salary
+    @observers = []
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify_observers
+    @observers.each { |observer| observer.update(self) }
+  end
+end
+
+# Payroll Class
+class Payroll
+  def update(changed_employee)
+    puts "#{changed_employee.name}'s new salary is #{changed_employee.salary}"
+  end
+end
+
+# Example code
+fred = Employee.new('Fred', 'Crane Operator', 30_000.0)
+payroll = Payroll.new
+fred.add_observer(payroll)
+
+fred.salary = 35_000.0
