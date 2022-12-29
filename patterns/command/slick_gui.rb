9e7e97d544ca1b1451898605e2dc49bff6a5468e
ayrton <ayrton.parkinson1@gmail.com>
2022-12-29 07:33:10 -0500

    Add explanatory comments

--- patterns/command/slick_gui.rb
+++ patterns/command/slick_gui.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 # The Command Pattern
+# With the command pattern, we construct objects that know how to perform
+# some very specific actions. In Ruby, we are able to use the command
+# pattern through concrete command classes or via code blocks (see _proc.rb
+# for example). Additionally, combined with the composite pattern, you can
+# queue commands, keep track of, and reverse commands if needed (see file_
+# command.rb for example)
+
+# Be careful not to use the command pattern when you can simply write out
+# the command and avoid useless boilerplate. Make sure that you really need
+# the command pattern before using!
 
 # SlickButton Class
 class SlickButton
