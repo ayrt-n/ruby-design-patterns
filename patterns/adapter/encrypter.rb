7dcafaa2ea169d40ad38993e6be7272ac3637c7d
ayrton <ayrton.parkinson1@gmail.com>
2023-01-09 06:13:10 -0500

    Add StringIOAdapter

--- patterns/adapter/encrypter.rb
+++ patterns/adapter/encrypter.rb
@@ -19,8 +19,33 @@ class Encrypter
   end
 end
 
-# Example code
+# String IO Adapter Class
+class StringIOAdapter
+  def initialize(string)
+    @string = string
+    @position = 0
+  end
+
+  def getc
+    raise EOFError if @position >= @string.length
+
+    @position += 1
+    @string[@position - 1]
+  end
+
+  def eof?
+    @position >= @string.length
+  end
+end
+
+# Example code using a file
 reader = File.open('./adapter/message.txt')
-writer = File.open('./adapter/message.encrypted', 'w')
+writer = File.open('./adapter/message_encrypted.txt', 'w')
 encrypter = Encrypter.new('my secret key')
+encrypter.encrypt(reader, writer)
+
+# Example code using our adapter
+encrypter = Encrypter.new('XYZZY')
+reader = StringIOAdapter.new('We attack at dawn')
+writer = File.open('./adapter/out.txt', 'w')
 encrypter.encrypt(reader, writer)
