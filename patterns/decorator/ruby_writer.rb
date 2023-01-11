ac6287d43df03e9114a2bd6d335569edaf25c06d
ayrton <ayrton.parkinson1@gmail.com>
2023-01-11 07:06:56 -0500

    Add example of module implementation of decorator

--- patterns/decorator/ruby_writer.rb
+++ patterns/decorator/ruby_writer.rb
@@ -16,3 +16,25 @@ class << w
 end
 
 w.write_line('Timestamp!!')
+
+# Example of module method of implementing decorators
+module TimeStampWriter
+  def write_line(line)
+    super("#{Time.new}: #{line}")
+  end
+end
+
+module NumberWriter
+  attr_reader :line_number
+
+  def write_line(line)
+    @line_number = 1 unless @line_number
+    super("#{@line_number}: #{line}")
+    @line_number += 1
+  end
+end
+
+w = SimpleWriter.new('module')
+w.extend(NumberWriter)
+w.extend(TimeStampWriter)
+w.write_line('Hello')
